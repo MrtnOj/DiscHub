88a589c3da2cef30f8e58988cc774c9e10a425f0
Martin Ojasalu <ojasalum@gmail.com>
2020-11-16 04:55:25 +0200

    ecosystem config change

--- DiscAPI/ecosystem.config.js
+++ DiscAPI/ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       production: {
         user: 'ubuntu',
         host: 'ec2-3-15-42-245.us-east-2.compute.amazonaws.com',
-        key: '~/.ssh/id_rsa.pub',
+        key: '~/.ssh/Discapp.pem',
         ref: 'origin/master',
         repo: 'git@github.com:MrtnOj/DiscHub.git',
         path: '/home/ubuntu/DiscApp/',
