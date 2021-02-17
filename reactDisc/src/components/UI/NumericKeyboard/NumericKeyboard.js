e58d6602c3bf10f59f097331ce7a91cc06ed1e5b
Martin Ojasalu <ojasalum@gmail.com>
2021-02-17 02:43:56 +0200

    bit more styling

--- reactDisc/src/components/UI/NumericKeyboard/NumericKeyboard.js
+++ reactDisc/src/components/UI/NumericKeyboard/NumericKeyboard.js
@@ -3,21 +3,19 @@ import React from 'react';
 import classes from './NumericKeyboard.module.css';
 
 const numericKeyboard = (props) => {
+    const keys = [
+        {label: "Prev", value: false}, {label: "0", value: 0}, {label: "Next", value: true},
+        {label: "1", value: 1}, {label: "2", value: 2}, {label: "3", value: 3}, {label: "4", value: 4},
+        {label: "5", value: 5}, {label: "6", value: 6}, {label: "7", value: 7}, {label: "8", value: 8}, 
+        {label: "9", value: 9}];
+
     return (
         <div className={classes.KeyboardContainer}>
             <div className={classes.Keyboard}>
-                <button onClick={() => props.arrowPressed(false)}>Prev</button>
-                <button onClick={()=> props.numberPressed(0)}>0</button>
-                <button onClick={() => props.arrowPressed(true)}>Next</button>
-                <button onClick={()=> props.numberPressed(1)}>1</button>
-                <button onClick={()=> props.numberPressed(2)}>2</button>
-                <button onClick={()=> props.numberPressed(3)}>3</button>
-                <button onClick={()=> props.numberPressed(4)}>4</button>
-                <button onClick={()=> props.numberPressed(5)}>5</button>
-                <button onClick={()=> props.numberPressed(6)}>6</button>
-                <button onClick={()=> props.numberPressed(7)}>7</button>
-                <button onClick={()=> props.numberPressed(8)}>8</button>
-                <button onClick={()=> props.numberPressed(9)}>9</button>
+                {keys.map(key => 
+                    <button 
+                        onClick={() => props.arrowPressed(key.value)}
+                        style={{cursor: 'pointer'}}>{key.label}</button>)}
             </div>
         </div>
     )
