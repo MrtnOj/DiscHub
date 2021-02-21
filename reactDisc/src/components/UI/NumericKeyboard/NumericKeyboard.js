2c6c912e0ae0108aa75628591f184b224c37adaa
Martin Ojasalu <ojasalum@gmail.com>
2021-02-21 21:57:41 +0200

    keyboard bug fix

--- reactDisc/src/components/UI/NumericKeyboard/NumericKeyboard.js
+++ reactDisc/src/components/UI/NumericKeyboard/NumericKeyboard.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './NumericKeyboard.module.css';
 
 const numericKeyboard = (props) => {
-    const keys = [
+    const holes = [
         {label: "Prev", value: false}, {label: "0", value: 0}, {label: "Next", value: true},
         {label: "1", value: 1}, {label: "2", value: 2}, {label: "3", value: 3}, {label: "4", value: 4},
         {label: "5", value: 5}, {label: "6", value: 6}, {label: "7", value: 7}, {label: "8", value: 8}, 
@@ -12,12 +12,13 @@ const numericKeyboard = (props) => {
     return (
         <div className={classes.KeyboardContainer}>
             <div className={classes.Keyboard}>
-                {keys.map(key => {
-                     const clickFunction = key.value === Boolean ? props.arrowPressed : props.numberPressed
+                {holes.map(hole => {
                      return (
                         <button 
-                            onClick={() => clickFunction(key.value)}
-                            style={{cursor: 'pointer'}}>{key.label}
+                            onClick={typeof hole.value === 'boolean' 
+                                ? () => props.arrowPressed(hole.value) 
+                                : () => props.numberPressed(hole.value)}
+                            style={{cursor: 'pointer'}}>{hole.label}
                         </button>)}
                      )
                 }
